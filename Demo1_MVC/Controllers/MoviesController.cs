0128df70ba6ea50280e9e570a38dd32af1b48ac5
AmiraAdel28 <amira.adel282001@gmail.com>
2025-03-07 23:47:21 +0200

    Action ReturnType

--- Demo1_MVC/Controllers/MoviesController.cs
+++ Demo1_MVC/Controllers/MoviesController.cs
@@ -4,9 +4,30 @@ namespace Demo1_MVC.Controllers
 {
     public class MoviesController : Controller
     {
-       public string GetMovie()
+        //public string GetMovie()
+        // {
+        //     return "Hello Route :) ";
+        // }
+
+        public ContentResult Index()
         {
-            return "Hello Route :) ";
+            ContentResult result = new ContentResult();
+            result.Content = "Black Adam";
+            return result;
+        }
+
+        public RedirectResult Index2()
+        {
+            RedirectResult result = new RedirectResult("http://localhost:39877/Movies/Index");
+            
+          return result;
+        }
+
+        public IActionResult Index3()
+        {
+            return RedirectToAction(nameof(Index));
+
+
         }
     }
 }
